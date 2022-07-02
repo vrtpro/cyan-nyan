cbcc5d195ff214c713b57cfd636d3a59c6d32287
BayuDC <dev.bayudc@gmail.com>
2022-07-02 12:23:07 +0700

    chore: better header

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,6 +1,7 @@
 const header = document.getElementById('header');
 const headerMenu = document.getElementById('header-menu');
 const headerNav = document.getElementById('header-nav');
+const main = document.getElementById('main');
 
 headerMenu.addEventListener('click', () => {
     if (!headerNav.classList.contains('active')) {
@@ -15,10 +16,36 @@ headerNav.addEventListener('animationend', () => {
     headerNav.classList.remove('close');
 });
 
-window.addEventListener('scroll', () => {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        header.classList.add('slim');
-    } else {
-        header.classList.remove('slim');
-    }
-});
+// window.addEventListener('scroll', () => {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         header.classList.add('slim');
+//     } else {
+//         header.classList.remove('slim');
+//     }
+// });
+
+new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                header.classList.remove('slim');
+            } else {
+                header.classList.add('slim');
+            }
+        });
+    },
+    { threshold: 1 }
+).observe(main.firstElementChild);
+
+new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                header.classList.remove('dark');
+            } else {
+                header.classList.add('dark');
+            }
+        });
+    },
+    { rootMargin: '-60px' }
+).observe(main.firstElementChild);
